5cea70a2d308ab062c1e37ebdd756b6386b6e177
Andres Galvan <andresdaniel.go2020@gmail.com>
2023-03-30 00:16:16 -0500

    Actualizando

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,27 @@
-export const FirstApp = () =>{
+//para que no see cree un div mas encima
+import {Fragment} from 'react'
+//<Fragment></Fragment>
+
+const newMessage = {
+    message: ' Hola mundo ',
+    title: 'Fernando '
+}
+
+const SentMessage  = () =>{
+    const message = "Hola mundo"
+    return message
+}
+
+export const FirstApp = () => {
+    
+    
+    //recuerda que el h1 entre {} no puedes
+    // mandar un objeto como tal
     return (
-        <h1>First App</h1>
+        <>
+            {/* <code>{ JSON.stringify(newMessage) }</code> */}
+            <h1>{ SentMessage() }</h1>
+            <p>Soy un subtitulo</p>
+        </>
     );
 }
