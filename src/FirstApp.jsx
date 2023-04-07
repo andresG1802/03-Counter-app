56b3009bfa7455b349ad2a413333c58fd40779da
andresG1802 <andresdaniel.go2020@gmail.com>
2023-04-06 21:03:12 -0500

    pushingCourseUpdate

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -3,24 +3,24 @@ import {Fragment} from 'react'
 //<Fragment></Fragment>
 
 const newMessage = {
-    message: ' Hola mundo ',
+    message: ' Andres ',
     title: 'Fernando '
 }
 
 const SentMessage  = () =>{
-    const message = "Hola mundo"
+    const message = "Andres"
     return message
 }
-
-export const FirstApp = () => {
+// Componente
+export const FirstApp = ( {title="Andres Galvan"} ) => {
     
-    
+    console.log(props);
     //recuerda que el h1 entre {} no puedes
     // mandar un objeto como tal
     return (
         <>
             {/* <code>{ JSON.stringify(newMessage) }</code> */}
-            <h1>{ SentMessage() }</h1>
+            <h1>Hola</h1>
             <p>Soy un subtitulo</p>
         </>
     );
